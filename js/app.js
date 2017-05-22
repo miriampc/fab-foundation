22f29263bbca563ae34a21635ef1bccc6c169c9d
Yelitza Cho <yelitza.choque@gmail.com>
2017-05-22 17:51:27 -0500

    Estructura details terminado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-$(document).foundation()
+var imgShow = document.getElementsByClassName("imgShow")[0];
+var changesImg = document.getElementById("changesImg");
+
+for(var i = 0; i < 4; i++){
+	var imgContent = document.getElementsByClassName("imgContent")[i];
+	var urlImg = "assets/img/wooden-teardrop-bg-s" + i +'.png';
+	imgContent.addEventListener("click", function(e){
+		if (e.target.tagName == "IMG") {
+      imgShow.style.display = "block";
+      console.log(e.target.src);
+      changesImg.src = e.target.src;
+		}
+	})
+}
