4cbcfb1bfdf9f7286881d68abe8df8897e88495a
Yelitza Cho <yelitza.choque@gmail.com>
2017-05-23 15:11:35 -0500

    carusel

--- js/details.js
+++ js/details.js
@@ -1,1 +1,26 @@
-$(document).foundation();
+$(document).foundation()
+$('.sim-thumb').on('mouseover', function() {
+  $('#main-product-image').attr('src', $(this).data('image'));
+})
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    navText: [
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>"
+      ],
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1100:{
+            items:5
+        }
+    }
+})
